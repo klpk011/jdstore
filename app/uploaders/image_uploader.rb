752ea7b45c7d0e50a854b9960310b7fa7c0e54ac
klpk011 <sophieQ011@gmail.com>
2017-04-16 11:21:48 +0800

    fix create image use qiniu

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #storage :file
   # storage :fog
   storage :qiniu
-
+  self.qiniu_can_overwrite = true
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
